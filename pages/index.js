8714665409126214894380c5f8f3893eb7314100
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-09-18 21:03:43 +0100

    added imports

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import axios from 'axios';
+import { useState } from 'react';
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Bodmin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
