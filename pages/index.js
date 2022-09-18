abab3c31a046d1eb2e314b36e73a1f219e3cb460
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-09-18 23:18:58 +0100

    Fixed styling

--- pages/index.js
+++ pages/index.js
@@ -34,23 +34,20 @@ export default function Home() {
         layout='fill'
         className='object-cover'
       />
-      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10'>
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
         <form
           onSubmit={fetchWeather}
-          className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-white text-white rounded-2xl'
+          className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'
         >
           <div>
             <input
               onChange={(e) => setCity(e.target.value)}
               className='bg-transparent border-none text-white focus:outline-none text-2xl'
               type='text'
-              placeholder='Search City'
+              placeholder='Search city'
             />
           </div>
-          <button
-            className='btn btn-primary border-none'
-            onClick={fetchWeather}
-          >
+          <button onClick={fetchWeather}>
             <MagnifyingGlassIcon className='w-6' />
           </button>
         </form>
