3fbf987d246b0215501951d7355304ac45c490a4
Jonjo Jefferson <jonjo.jefferson@gmail.com>
2022-09-18 21:01:43 +0100

    finished setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import Navbar from "../components/Navbar";
+import Head from 'next/head';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Bodmin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <Head>
+        <title>Jonjo.tech</title>
+      </Head>
     </div>
   );
 }
